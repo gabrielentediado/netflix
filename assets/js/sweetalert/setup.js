d4b4b346ad475d105d9e4409530ae849c288b871
gabrielentediado <gabrielcardosopassos@gmail.com>
2024-11-12 10:07:33 -0300

    .

--- assets/js/sweetalert/setup.js
+++ assets/js/sweetalert/setup.js
@@ -1,14 +1,14 @@
-const infoFilesX = document.querySelector("#info-files-x");
+const infoFilesX = document.querySelector("#info");
 
 infoFilesX.addEventListener("click", function () {
   Swal.fire({
     title: "Stranger Things",
     text:
-      "Quando um rapaz desaparece, uma pequena vila descobre um mistério relacionado com experiências secretas, assustadoras forças sobrenaturais e uma estranha rapariga. Série inspirada nos clássicos de terror dos anos 80 nomeada para vários prémios Emmy, incluindo três de Melhor Drama.",
+      "Quando um rapaz desaparece, uma pequena vila descobre um mistério relacionado com experiências secretas, assustadoras forças sobrenaturais e uma estranha criança. Série inspirada nos clássicos de terror dos anos 80 nomeada para vários prémios Emmy, incluindo três de Melhor Drama.",
     imageUrl: "./assets/img/Stranger_Things_Season_1.png",
     imageWidth: 200,
     imageHeight: 300,
-    imageAlt: "Stanger thing",
+    imageAlt: "Stanger Things",
     theme: "terror",
   });
 });
